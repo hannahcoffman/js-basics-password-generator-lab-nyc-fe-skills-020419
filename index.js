90bb766fce94cf8b3e54b302a3de8340c1b67472
Hannah Coffman <hcoffman7@gmail.com>
2019-04-01 23:34:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  
+  function onSubmit(event){
+    console.log('hi')
+    event.preventDefault()
+    const input =document.getElementById('password-length')
+    console.log(input.value)
+  }
+
+  document.addEventListener('submit', onSubmit)
+    
 });
