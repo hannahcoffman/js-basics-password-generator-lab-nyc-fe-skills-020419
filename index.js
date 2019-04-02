91f3bb347d53066dd97f9704da3205a0fa42afd1
Hannah Coffman <hcoffman7@gmail.com>
2019-04-02 00:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
   const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
   const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   const numbers = "0123456789"
-  const specialCharacters = ""
+  const specialCharacters = "!#$%&()*+,-./:;<=>?@[\]^_{|}~"
   
   function onSubmit(event){
     event.preventDefault()
@@ -15,8 +15,18 @@ window.addEventListener("load", () => {
     let startingString = ''
     
     for (let i = 0; i <howManyCharacters; i++) {
-    const randomIndex = Math.floor(Math.random() * 26)
-    startingString += lowerCaseLetters[randomIndex]
+    let randomIndex = Math.floor(Math.random() * lowerCaseLetters.length)
+    startingString += lowerCaseLetters[randomIndex] 
+    
+    randomIndex = Math.floor(Math.random() * upperCaseLetters.length)
+    startingString += upperCaseLetters[randomIndex] 
+    
+    randomIndex = Math.floor(Math.random() * numbers.length)
+    startingString += numbers[randomIndex] 
+    
+    randomIndex = Math.floor(Math.random() * specialCharacters.length)
+    startingString += specialCharacters[randomIndex] 
+    
     }
    const output = document.getElementById('result')
    output.innerHTML = startingString
